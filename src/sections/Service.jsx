0577cc8c9b2fec67c0dcf2c2eca8dc28c103628a
NikDemik <nikf24@mail.ru>
2025-06-19 18:00:43 +0300

    add sec

--- src/sections/Service.jsx
+++ src/sections/Service.jsx
@@ -5,9 +5,20 @@ const Service = () => {
         <section>
             <Element name="service">
                 <div className="container-full-l">
-                    <div className="container bg2">
-                        <div className=" relative mt-44 grid-cols-2">
-                            <h1 className=" h1 pb-24 pt-24">SERVICE Lorem ipsum dolor sit</h1>
+                    <div className="container-r">
+                        <div className=" relative mt-44 grid grid-cols-2">
+                            <div>
+                                <ul className=" flex flex-col gap-3">
+                                    <li className=" section-title section-title-bg">
+                                        ФИРМЕННЫЙ СТИЛЬ
+                                    </li>
+                                    <li className=" section-title section-title-bg">ПОЛИГРАФИЯ</li>
+                                    <li className=" section-title section-title-bg">РЕКЛАМА</li>
+                                    <li className=" section-title section-title-bg">САЙТЫ</li>
+                                    <li className=" section-title section-title-bg">МЕРЧ</li>
+                                </ul>
+                            </div>
+                            <h1 className=" h1 pb-24 pt-24 bg3">SERVICE Lorem ipsum dolor sit</h1>
                         </div>
                     </div>
                 </div>
